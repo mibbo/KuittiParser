b71a60e4a60275c9d2998189e9ed801a4f024f63
Tommi Mikkola <tommi.mikkola@cgi.com>
2023-01-09 09:04:04 +0200

    Create functionality for parsing rows from receipt

---
+++ KuittiParser/ProductModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KuittiParser
+{
+    internal class ProductModel
+    {
+        public string Name { get; set; }
+        private decimal costField;
+        public decimal Cost
+        {
+            get
+            {
+                return this.costField;
+            }
+            set
+            {
+                //if (this.costField < 0)
+                //{
+                //    this.costField = this.costField - value;
+
+                //}
+                this.costField += value;
+            }
+        }
+
+    }
+
+
+}
---
+++ KuittiParser/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using KuittiParser;
+using System.Collections.Generic;
+using System.Data;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+
+using (PdfDocument document = PdfDocument.Open(@"C:\Users\tommi.mikkola\git\Projektit\KuittiParser\KuittiParser\Kuitit\testikuitti.pdf"))
+{
+    foreach (Page page in document.GetPages())
+    {
+        var wordList = page.GetWords().ToList();
+
+        // Create Lists of words for each receipt row number
+        List<List<Word>> rowList = wordList.GroupBy(it => it.BoundingBox.Bottom).Select(grp => grp.ToList()).ToList();
+
+        foreach (var rowWords in rowList.Skip(4))
+        {
+            if (rowWords.LastOrDefault().Letters.Reverse().Skip(2).FirstOrDefault().Value != ",")
+                continue;
+            if (rowWords.FirstOrDefault().Text == "YHTEENSÄ")
+            {
+                break;
+            }
+
+            var words = rowWords.Select(word => word.Text).ToList();
+            var productWords = words.Remove(words.LastOrDefault());
+
+            var product = new ProductModel
+            {
+                Name = string.Join(" ", words)
+            };
+
+            Console.WriteLine(product.Name);
+        }
+
+        //foreach (Word word in page.GetWords())
+        //{
+        //    if (word.BoundingBox.Height == previousWord.BoundingBox.Height)
+        //    {
+
+        //    }
+        //    else
+        //    {
+        //        previousWord = word;
+        //    }
+        //    Console.WriteLine(word.Text);
+        //}
+    }
+}
